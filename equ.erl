6387d2212a1156b50157d5a55a81655c48f1755b
Jan <jan.deinhard@gmail.com>
2010-11-14 23:14:13 +0100

    improved code

--- equ.erl
+++ equ.erl
@@ -1,42 +1,41 @@
 -module(equ).
--export([start/0, start/4]).
--export([connect/3]).
+-export([start/0]).
 
 start() ->
-  start(16, 1234, 'localhost', 80).
+  init(16, 1234, 'localhost', 80).
 
-start(Num, InPort, OutHost, OutPort) ->
+init(Num, InPort, OutHost, OutPort) ->
   case  gen_tcp:listen(InPort, [binary, {packet, 0}, {active, once}]) of
     {ok, Listen} ->
-      start_servers(Num, Listen, OutHost, OutPort);
+      start_acceptors(Num, Listen, OutHost, OutPort);
     {error, Reason} ->
       {error, Reason}
   end.
   
-start_servers(0, _, _, _) ->
+start_acceptors(0, _, _, _) ->
   ok;
-start_servers(Num, Listen, OutHost, OutPort) ->
-  spawn(equ, connect, [Listen, OutHost, OutPort]),
-  start_servers(Num-1, Listen, OutHost, OutPort).
+start_acceptors(Num, Listen, OutHost, OutPort) ->
+  spawn(fun() -> acceptor(Listen, OutHost, OutPort) end),
+  start_acceptors(Num-1, Listen, OutHost, OutPort).
 
-connect(Listen, OutHost, OutPort) ->
+acceptor(Listen, OutHost, OutPort) ->
   {ok, Client} = gen_tcp:accept(Listen),
-  spawn(fun() -> connect(Listen, OutHost, OutPort) end),
+  spawn(fun() -> acceptor(Listen, OutHost, OutPort) end),
   {ok, Server} = gen_tcp:connect(OutHost, OutPort, [binary, {packet, 0}, {active, once}]),
-  loop(Client, Server).
+  proxy_loop(Client, Server).
 
-loop(Client, Server) ->
+proxy_loop(Client, Server) ->
   receive
     {tcp, Client, Data} ->
       gen_tcp:send(Server, Data),
       inet:setopts(Client, [{active, once}]),
-      loop(Client, Server);
+      proxy_loop(Client, Server);
     {tcp, Server, Data} ->
       gen_tcp:send(Client, Data),
       inet:setopts(Server, [{active, once}]),
-      loop(Client, Server);
+      proxy_loop(Client, Server);
     {tcp_closed, _} ->
       ok;
     _ ->
-      error
+      {error, unknown_message}
   end.
