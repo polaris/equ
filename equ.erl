f407ffd0772858a893772b614acb0850e8ecabb5
Jan <jan.deinhard@gmail.com>
2010-11-14 22:59:56 +0100

    added acceptor pooling

--- equ.erl
+++ equ.erl
@@ -1,9 +1,23 @@
--module(proxy).
--export([start/3]).
+-module(equ).
+-export([start/0, start/4]).
+-export([connect/3]).
 
-start(InPort, OutHost, OutPort) ->
-  {ok, Listen} = gen_tcp:listen(InPort, [binary, {packet, 0}, {active, once}]),
-  spawn(fun() -> connect(Listen, OutHost, OutPort) end).
+start() ->
+  start(16, 1234, 'localhost', 80).
+
+start(Num, InPort, OutHost, OutPort) ->
+  case  gen_tcp:listen(InPort, [binary, {packet, 0}, {active, once}]) of
+    {ok, Listen} ->
+      start_servers(Num, Listen, OutHost, OutPort);
+    {error, Reason} ->
+      {error, Reason}
+  end.
+  
+start_servers(0, _, _, _) ->
+  ok;
+start_servers(Num, Listen, OutHost, OutPort) ->
+  spawn(equ, connect, [Listen, OutHost, OutPort]),
+  start_servers(Num-1, Listen, OutHost, OutPort).
 
 connect(Listen, OutHost, OutPort) ->
   {ok, Client} = gen_tcp:accept(Listen),
@@ -22,5 +36,7 @@ loop(Client, Server) ->
       inet:setopts(Server, [{active, once}]),
       loop(Client, Server);
     {tcp_closed, _} ->
-      ok
+      ok;
+    _ ->
+      error
   end.
