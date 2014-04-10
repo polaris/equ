e5db979907b06f9517f6838c8414e583f1f724e5
Jan Deinhard <jan.deinhard@gmail.com>
2014-04-10 21:51:48 +0200

    prepare app for testing

--- src/equ_app.erl
+++ src/equ_app.erl
@@ -4,8 +4,20 @@
 
 -export([start/2, stop/1]).
 
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
 start(_Type, _StartArgs) ->
   equ_sup:start_link().
 
 stop(_State) ->
   ok.
+
+-ifdef(TEST).
+
+simple_test() ->
+  ok = application:start(equ),
+  ?assertNot(undefined == whereis(equ_sup)).
+
+-endif.
